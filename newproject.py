b9f4664a15577770702d9b9c72c102d345d5f65e
Sajina P <sajinamusthafa567@gmail.com>
2025-09-11 17:31:56 +0530

    initial commit

---
+++ newproject.py
@@ -0,0 +1,14 @@
+# Examples of functions
+
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def mul(a,b):
+    return a * b
+
+print("addition :", add(10, 5))
+print("subtraction:", sub(10, 2))
+print("multiplication:", mul(2, 3))
